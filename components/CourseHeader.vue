e6346113ec93588c8312ef438f99f964ce293e61
wakataw <agungpratama1001@gmail.com>
2022-12-06 15:39:28 +0700

    Add detil ujian page

--- components/CourseHeader.vue
+++ components/CourseHeader.vue
@@ -1,16 +1,37 @@
 <template>
-  <div class="h-48 bg-[url('~/assets/images/blue-abstract.svg')] px-4">
-    <div class="max-w-screen-xl mx-auto w-full h-full flex flex-col ">
-      <div class="h-2/3 items-center flex">
+  <div class="bg-[url('~/assets/images/blue-abstract.svg')] bg-cover px-4 py-8">
+    <div class="max-w-screen-xl mx-auto w-full h-full py-4 flex space-x-24">
+      <div class="w-1/2 space-y-4">
         <h1 class="text-2xl font-semibold text-white">Uji Kompetensi Kenaikan Jenjang Fungsional Pemeriksa Pajak</h1>
+        <div class="text-slate-400 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim minima molestiae omnis praesentium quae quisquam. Adipisci architecto asperiores aspernatur assumenda esse expedita fugit in modi, nemo obcaecati officiis perferendis perspiciatis quae quaerat, quo quod repellendus repudiandae sint ut vitae! At consequuntur cumque deleniti dignissimos doloremque dolores eveniet laborum sint velit?</div>
+      </div>
+      <div class="flex flex-col justify-between w-1/2">
+        <DescriptionItem title="Waktu" subtitle="90 Menit">
+          <ClockIcon class="w-8 h-8"></ClockIcon>
+        </DescriptionItem>
+        <DescriptionItem title="Paket" subtitle="1 tes">
+          <ClipboardDocumentCheckIcon class="w-8 h-8"></ClipboardDocumentCheckIcon>
+        </DescriptionItem>
+        <DescriptionItem title="Jadwal" subtitle="6 Desember 2022">
+          <CalendarDaysIcon class="w-8 h-8"></CalendarDaysIcon>
+        </DescriptionItem>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { CalendarDaysIcon, ClockIcon, ClipboardDocumentCheckIcon } from '@heroicons/vue/24/outline'
+import DescriptionItem from "./course/DescriptionItem";
+
 export default {
-  name: "CourseHeader"
+  name: "CourseHeader",
+  components: {
+    DescriptionItem,
+    ClockIcon,
+    CalendarDaysIcon,
+    ClipboardDocumentCheckIcon
+  }
 }
 </script>
 
--- pages/ujian/[id]/index.vue
+++ pages/ujian/[id]/index.vue
@@ -1,15 +1,18 @@
 <template>
-  <h1>this is id {{ route.params.id }}</h1>
+  <div class="max-w-screen-xl mx-auto py-4 space-y-4">
+    <h2 class="text-2xl font-semibold underline decoration-yellow-400 decoration-4">Daftar Ujian</h2>
+    <article class="max-w-screen-xl mx-auto grid px-4 xl:px-0 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 my-4 gap-4">
+      <modul-v-card :random-id="i" v-for="i in [1, 2]" ></modul-v-card>
+    </article>
+  </div>
 </template>
 
 <script setup>
-  import {useRoute} from "nuxt/app";
+  import ModulVCard from "../../../components/ModulVCard";
 
   definePageMeta({
     layout: "course",
   })
-
-  const route = useRoute();
 
 </script>
 
